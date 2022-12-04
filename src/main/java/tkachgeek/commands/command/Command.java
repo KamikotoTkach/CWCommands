640eddde20dec75e37a34b188d171a240b8e2fd9
DESKTOP\PC <2_D_Tkach@mail.ru>
2022-12-04 13:39:29 +0300

    pre-release с абсолютными пермами

--- src/main/java/tkachgeek/commands/command/Command.java
+++ src/main/java/tkachgeek/commands/command/Command.java
@@ -51,6 +51,7 @@ public class Command {
     this(name);
     arguments(new ArgumentSet(executor, name));
   }
+  
   public Command(String name, Executor executor, Argument... arguments) {
     this(name);
     arguments(new ArgumentSet(executor, name, arguments));
@@ -83,6 +84,9 @@ public class Command {
         permission = permissions + "." + name; //если подкоманда и пермишен не указан
       } else {
         if (!permission.isEmpty()) {
+          if (permission.startsWith("$")) { //если пермишен начинается с $
+            permission = permission.substring(1);
+          }
           permission = permissions + "." + permission; //если не подкоманда и пермишен указан
         }
       }
@@ -98,6 +102,9 @@ public class Command {
     if (permission != null && !permission.isEmpty()) {
       for (ArgumentSet argumentSet : argumentSets) {
         if (argumentSet.permission != null && !argumentSet.permission.isEmpty()) {
+          if(argumentSet.permission.startsWith("$")) {
+            argumentSet.permission = argumentSet.permission.substring(1); //если пермишен начинается с $
+          }
           argumentSet.permission = permissions + "." + argumentSet.permission; //если пермишен указан и не пустой
         } else {
           argumentSet.permission = ""; //если пермишен не указан или пустой
@@ -254,7 +261,7 @@ public class Command {
       }
       sender.sendMessage("");
     }
-  
+    
     sender.sendMessage("");
     if (toSend.isEmpty()) {
       sender.sendMessage(Component.text("Для вас нет доступных продолжений этой команды", text));
