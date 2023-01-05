2e3a87a123347ca0229a43e8eb2a49ed489888dd
Soul_KRT <soul.krt.00@gmail.com>
2023-01-05 12:53:54 +0300

    add SoundArg, ParticleArg

---
+++ src/main/java/tkachgeek/commands/command/arguments/SoundArg.java
@@ -0,0 +1,28 @@
+package tkachgeek.commands.command.arguments;
+
+import org.bukkit.Sound;
+import org.bukkit.command.CommandSender;
+import tkachgeek.commands.command.Argument;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SoundArg extends Argument {
+    static List<String> sounds = Arrays.stream(Sound.values()).map(Enum::name).collect(Collectors.toList());
+
+    @Override
+    public boolean valid(String raw) {
+        return sounds.contains(raw);
+    }
+
+    @Override
+    public List<String> completions(CommandSender sender) {
+        return sounds;
+    }
+
+    @Override
+    public String argumentName() {
+        return "материал";
+    }
+}
