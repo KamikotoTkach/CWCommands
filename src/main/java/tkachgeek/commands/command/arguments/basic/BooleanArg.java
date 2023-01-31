03316b494a04632e77b6d8b3a739c260763117b5
Soul_KRT <soul.krt.00@gmail.com>
2023-01-31 22:54:25 +0300

    update BooleanArg

--- src/main/java/tkachgeek/commands/command/arguments/basic/BooleanArg.java
+++ src/main/java/tkachgeek/commands/command/arguments/basic/BooleanArg.java
@@ -7,59 +7,67 @@ import java.util.Arrays;
 import java.util.List;
 
 public class BooleanArg extends Argument {
-  final String trueValue;
-  final String falseValue;
-  
+  private String trueValue;
+  private String falseValue;
+
   public BooleanArg() {
     this("true", "false");
   }
-  
+
   public BooleanArg(String trueValue, String falseValue) {
     this.trueValue = trueValue;
     this.falseValue = falseValue;
   }
-  
+
   @Override
   public Integer toInt() {
     if (valid()) return raw.equalsIgnoreCase(trueValue) ? 1 : 0;
     return null;
   }
-  
+
   @Override
   public String toString() {
     if (valid()) return raw.equalsIgnoreCase(trueValue) ? "true" : "false";
     return null;
   }
-  
+
   @Override
   public Double toDouble() {
     if (valid()) return raw.equalsIgnoreCase(trueValue) ? 1.0 : 0.0;
     return null;
   }
-  
+
   @Override
   public Boolean toBoolean() {
     if (valid()) return raw.equalsIgnoreCase(trueValue);
     return null;
   }
-  
+
   @Override
   public boolean valid(String raw) {
     return raw.equalsIgnoreCase(trueValue) || raw.equalsIgnoreCase(falseValue);
   }
-  
+
   @Override
   public List<String> completions(CommandSender sender) {
     return Arrays.asList(trueValue, falseValue);
   }
-  
+
   @Override
   public String argumentName() {
     return trueValue + "/" + falseValue;
   }
-  
+
   @Override
   protected String hint() {
     return trueValue + " или " + falseValue;
+  }
+
+  public void setTrueValue(String trueValue) {
+    this.trueValue = trueValue;
+  }
+
+  public void setFalseValue(String falseValue) {
+    this.falseValue = falseValue;
   }
 }
