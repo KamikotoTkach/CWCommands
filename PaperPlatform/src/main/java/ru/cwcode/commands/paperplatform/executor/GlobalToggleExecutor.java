9de63161af66cc2fd1d8d3db4ac044c1598536db
DESKTOP\PC <tkach@cwcode.ru>
2023-11-20 19:36:29 +0300

    fix(PaperPlatform): GlobalToggleExecutor, LocalTimedExecutor, LocalToggleExecutor

--- PaperPlatform/src/main/java/ru/cwcode/commands/paperplatform/executor/GlobalToggleExecutor.java
+++ PaperPlatform/src/main/java/ru/cwcode/commands/paperplatform/executor/GlobalToggleExecutor.java
@@ -1,10 +1,70 @@
 package ru.cwcode.commands.paperplatform.executor;
 
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import ru.cwcode.commands.api.CommandsAPI;
 import ru.cwcode.commands.executor.AbstractGlobalToggleExecutor;
+import ru.cwcode.commands.paperplatform.paper.PaperSender;
+import tkachgeek.tkachutils.messages.MessageReturn;
+import tkachgeek.tkachutils.messages.TargetableMessageReturn;
+
+import java.util.Optional;
 
 public abstract class GlobalToggleExecutor extends AbstractGlobalToggleExecutor {
-  
   public GlobalToggleExecutor(boolean initialState) {
     super(initialState);
+  }
+  
+  @Override
+  public void errorHandler(Exception exception) { //todo: убрать дублирование каким-то образом
+    if (exception instanceof MessageReturn) {
+      MessageReturn messageReturn = (MessageReturn) exception;
+      if (messageReturn.isStyled()) {
+        sender.sendMessage(messageReturn.getComponentMessage());
+      } else {
+        sender.sendMessage(messageReturn.getComponentMessage()
+                                        .color(command.getColorScheme().main()));
+      }
+      return;
+    } else if (exception instanceof TargetableMessageReturn) {
+      sender.sendMessage(((TargetableMessageReturn) exception).getMessage(((PaperSender) sender).getCommandSender()));
+      return;
+    }
+    
+    String localizedMessage = exception.getLocalizedMessage();
+    if (localizedMessage == null) localizedMessage = "Неизвестная ошибка";
+    
+    sender.sendMessage(Component.text(localizedMessage, command.getColorScheme().main()));
+    
+    CommandsAPI.getPlatform().getLogger().warn("Ошибка при исполнении " + this.getClass().getName());
+    exception.printStackTrace();
+  }
+  
+  @Override
+  protected Audience sender() {
+    return sender.getAudience();
+  }
+  
+  @Override
+  protected Optional<Player> argP(int index) {
+    Player player = Bukkit.getPlayer(argS(index));
+    if (player == null) return Optional.empty();
+    
+    return Optional.of(player);
+  }
+  
+  protected final Player player() {
+    if (sender instanceof PaperSender) {
+      return ((PaperSender) sender).getPlayer();
+    }
+    
+    return null;
+  }
+  
+  protected final Material argM(int index) {
+    return Material.matchMaterial(argS(index));
   }
 }
