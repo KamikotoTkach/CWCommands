9bb71e73d5b68961e276089860fcd7d0ad3a9ab1
Soul_KRT <soul.krt.00@gmail.com>
2026-04-01 17:01:48 +0300

    fix PlayerArg

--- PaperPlatform/src/main/java/ru/cwcode/commands/paperplatform/argument/PlayerArg.java
+++ PaperPlatform/src/main/java/ru/cwcode/commands/paperplatform/argument/PlayerArg.java
@@ -1,11 +1,20 @@
 package ru.cwcode.commands.paperplatform.argument;
 
+import ru.cwcode.commands.api.Sender;
+
+import java.util.List;
+
 import static ru.cwcode.commands.api.CommandsAPI.l10n;
 
 public class PlayerArg extends OnlinePlayers {
   @Override
   public boolean valid(String raw) {
     return raw.matches("^\\w{3,16}$");
+  }
+  
+  @Override
+  public boolean valid(Sender sender, String raw, List<String> arguments) {
+    return valid(raw);
   }
   
   @Override
